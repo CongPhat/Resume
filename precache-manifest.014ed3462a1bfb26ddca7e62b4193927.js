0127f39904d48a4834b29e1012b02b356874167d
CongPhat <huynhcongphat2510@gmail.com>
2019-07-24 19:09:42 +0700

    Summary
    Resume

---
+++ precache-manifest.014ed3462a1bfb26ddca7e62b4193927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0390b50dabb476a4d1bfc213790de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9451eff2d4a88efd12",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8a9451eff2d4a88efd12",
+    "url": "/static/js/main.bdcdd214.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
